2e323b9b959754549d63833fc9754b9e5892548f
Filip Stangricki <stangricki@gmail.com>
2018-05-10 10:06:16 +0200

    Starter Pack Finished

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ const gulp = require('gulp'),
 
 gulp.task('sass', function() {
 	gulp.src(['./node_modules/bootstrap/scss/bootstrap.scss', './src/scss/**/*.scss'])
-		.pipe(sass().on('error', sass.logError))
+		.pipe(sass().on('error', function(errorInfo){
+			console.log(errorInfo.toString());
+			this.emit('end')
+		}))
 		.pipe(gulp.dest('./src/css'))
 		.pipe(browserSync.stream());
 })
@@ -14,3 +17,32 @@ gulp.task('js', function() {
 		.pipe(gulp.dest('./src/js'))
 		.pipe(browserSync.stream());
 })
+
+gulp.task('serve', ['sass'], function(){
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "./src"
+		}
+	})
+
+	gulp.watch(['./node_modules/bootstrap/scss/bootstrap.scss', './src/scss/**/*.scss'], function(){
+		gulp.start('sass')
+	});
+
+	gulp.watch("./src/*.html", function(){
+		browserSync.reload()
+	})
+})
+
+gulp.task('fonts', function(){
+	gulp.src('./node_modules/font-awesome/fonts/*')
+		.pipe(gulp.dest("./src/fonts"))
+})
+
+gulp.task('fa', function(){
+	gulp.src('./node_modules/font-awesome/css/font-awesome.min.css')
+		.pipe(gulp.dest('src/css'))
+})
+
+gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
